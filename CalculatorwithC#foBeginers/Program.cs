58df0c7ca2bba9f237f8e483c4255fe574dc077b
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-12-01 20:03:44 +0100

    Add files via upload

---
+++ CalculatorwithC#foBeginers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace Calculator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Insert the first number:");
+            string firstInput = Console.ReadLine();
+            int firstNumber = int.Parse(firstInput);
+
+            Console.WriteLine("Insert the second number:");
+            string secondInput = Console.ReadLine();
+            int secondNumber = int.Parse(secondInput);
+
+            int sum = firstNumber + secondNumber;
+            Console.WriteLine($"Sum of numbers = {sum}");
+        }
+    }
+}
